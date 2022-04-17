18a38ce76da80db5ae0024231a99795d75c1bf2b
Ed L <beartechtalks@gmail.com>
2022-04-17 20:02:18 +0100

    fix?: move d.ts

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Client } from "revolt.js";
-
-import { Message } from "revolt.js/dist/maps/Messages";
-
-export function archive(client: Client, msg: Message, botMsg?: Message): string;
---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+import { Client } from "revolt.js";
+
+import { Message } from "revolt.js/dist/maps/Messages";
+
+export function archive(
+  client: Client,
+  msg: Message,
+  botMsg?: Message
+): Promise<string>;
