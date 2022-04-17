f9519e138d61181c09a37582c038188c67d4b3ec
sportshead <32637656+sportshead@users.noreply.github.com>
2022-04-18 03:57:19 +0800

    fix: fix types, change to default export (#1)
    * Update package.json
    
    * Delete global.d.ts
    
    * Update index.ts

--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "revolt.js";
-
-import { Message } from "revolt.js/dist/maps/Messages";
-
-export default function archive(
-  client: Client,
-  msg: Message,
-  botMsg?: Message
-): Promise<string>;
